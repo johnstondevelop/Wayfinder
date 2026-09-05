3fbd3aabe4a3aa4d6009d32891b1b7b6cc374211
Patrick Johnston <286977277+johnstondevelop@users.noreply.github.com>
2026-09-05 01:46:25 -0400

    Adding stops

--- Maptoken-categories/maptoken-config.js
+++ Maptoken-categories/maptoken-config.js
@@ -3,4 +3,23 @@ export const MAPBOX_TOKEN = 'pk.eyJ1Ijoiam9obnN0b25kZXZlbG9wIiwiYSI6ImNtdG54MHlz
 export const DEFAULT_CENTER = [-98.5795, 39.8283];
 export const DEFAULT_ZOOM = 3.5;
 
-export const ROMANTIC_POI_CATEGORIES = ['scenic overlook', 'waterfalls', 'lighthouses', 'wineries', 'beaches', 'gardens', 'trails', 'parks'];
+export const ROMANTIC_POI_CATEGORIES = ['viewpoint',
+     'waterfall',
+      'lighthouse',
+       'winery',
+        'beach',
+         'garden',
+          'trailhead',
+           'park',
+            'historic_site',
+             'castle',
+              'monument',
+               'cinema',
+                'theater',
+                 'art_gallery',
+                  'museum',
+                   'diner_restaurant',
+                    'dessert_shop',
+                     'pier',
+                      'cafe',
+                       'wine_bar'];
